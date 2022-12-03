734fa3ed97cd2f4405b534545073d7b4586972e4
FH-16 <23100071@lums.edu.pk>
2022-12-03 22:46:50 +0500

    Part 9 and 10

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     private Vector3 direction;
     public float forwardSpeed;
 
+    //Move Left Right 
+    private int desireLane = 1; //Default is -2, 1 is left, -4 is right
+    public float laneDistance = 3; 
+    public float laneSpeed = 100;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,7 +22,37 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction.z = -forwardSpeed;
-    }
+
+        //Lane 
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+            desireLane++; //one press
+            if(desireLane == 3) //two press 
+                desireLane = 2;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desireLane--; //one press
+            if(desireLane == -1) //two press 
+                desireLane = 0;
+        }
+        //Calculate where we should be 
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(desireLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desireLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+
+        }
+
+        
+        transform.position = Vector3.Lerp(transform.position, targetPosition, laneSpeed* Time.deltaTime);
+
+    } 
 
     private void FixedUpdate()
     {
