79fcf9e245a64fbdaa22d3038aebf7c1d06d9d26
Bakhtiar Rasheed <22100267@lums.edu.pk>
2022-12-04 19:40:54 +0500

    done till part 14

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,55 +7,125 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
+    public float maxSpeed;
 
     //Move Left Right 
-    private int desireLane = 1; //Default is -2, 1 is left, -4 is right
+    private int desireLane = 0; //Default is -2, 1 is left, -4 is right
     public float laneDistance = 3; 
     public float laneSpeed = 100;
+
+    //Jump code
+    public Animator anim;
+    public float jumpForce = 10;
+    public float gravity = -20;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        direction.z = -forwardSpeed;
+
         
     }
 
     void Update()
     {
-        direction.z = -forwardSpeed;
+
 
         //Lane 
         if(Input.GetKeyDown(KeyCode.LeftArrow)) 
         {
-            desireLane++; //one press
-            if(desireLane == 3) //two press 
-                desireLane = 2;
+             if(desireLane==1){
+                desireLane=0;
+            }
+            else if(desireLane ==0) //two press 
+                {desireLane +=1;}
+            else if(desireLane ==-1) //two press 
+                {desireLane +=1;}
+            else if (desireLane == -2) //two press 
+            { desireLane += 1; }
+
+
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            desireLane--; //one press
-            if(desireLane == -1) //two press 
-                desireLane = 0;
-        }
-        //Calculate where we should be 
-        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+            // if(desireLane <1) //two press 
+            //     desireLane -= 1;
+             if(desireLane==-2){
+                desireLane=0;
+            }
+       
+                     else if(desireLane ==0) //two press 
+                {desireLane -=1;}
 
-        if(desireLane == 0)
+                   else if(desireLane ==1) //two press 
+                {desireLane -=1;}
+            else if (desireLane == -1) //two press 
+            { desireLane -= 1; }
+
+
+
+
+
+
+
+
+
+        }
+
+        Vector3 targetPosition = transform.position;
+         targetPosition.x = desireLane;
+         transform.position=targetPosition;
+
+         transform.Rotate(Vector3.up,0.0f);
+
+        //Jump code
+        direction.y += gravity * Time.deltaTime;
+        if (controller.isGrounded)
         {
-            targetPosition += Vector3.left * laneDistance;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+       
         }
-        else if (desireLane == 2)
+
+        //Slider Input
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            targetPosition += Vector3.right * laneDistance;
-
+            StartCoroutine(Slider());
         }
 
-        
-        transform.position = Vector3.Lerp(transform.position, targetPosition, laneSpeed* Time.deltaTime);
 
-    } 
+
+    } //end update function
 
     private void FixedUpdate()
     {
         controller.Move(direction*Time.fixedDeltaTime);
     }
-}
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+        anim.SetTrigger("JumpTrigger");       
+    }
+
+    //Slider Function
+    IEnumerator Slider()
+    {
+        anim.SetTrigger("SliderT");
+        //controller.height = 1;
+        //controller.center = new Vector3(0, -1f, 0);
+        yield return new WaitForSeconds(2f);
+        //controller.height = 2;
+        //controller.center = new Vector3(0, -1, 0);
+        if (forwardSpeed < maxSpeed)
+        {
+            forwardSpeed += 0.2f * Time.deltaTime;
+        }
+
+    }
+
+
+
+}                                                         
